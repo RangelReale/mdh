c702d7d6c6666843c7d27c3ba57c94bcc58d6286
Rangel Reale <rangelspam@gmail.com>
2016-12-08 11:38:41 -0200

    * Allows convert to ignore 'from' or 'to'

--- BaseMDH.php
+++ BaseMDH.php
@@ -15,13 +15,25 @@ class BaseMDH
     public $dateFormat = IDataHandler::FORMAT_SHORT;
     public $timeFormat = IDataHandler::FORMAT_SHORT;
     public $dateTimeFormat = IDataHandler::FORMAT_SHORT;
-    
+
+    /**
+     * Constructor
+     */
     public function __construct()
     {
         $this->_converters['default'] = new DefaultConverter($this);
     }
     
-    // convert the value from handler format to PHP format
+    /**
+     * Convert the value to PHP format using the converter
+     * 
+     * @param string $converter converter name
+     * @param string $datatype data type
+     * @param mixed $value value
+     * @param array $options options
+     * @return mixed parsed value in PHP format
+     * @throws InvalidConverterException
+     */
     public function parse($converter, $datatype, $value, $options = [])
     {
         if ($converter == '') $converter = 'default';
@@ -35,7 +47,16 @@ class BaseMDH
         throw new InvalidConverterException($converter);
     }
     
-    // convert the value from php format to handler format
+    /**
+     * Convert the value from PHP format using the converter
+     * 
+     * @param string $converter converter name
+     * @param string $datatype data type
+     * @param mixed $value value
+     * @param array $options options
+     * @return mixed formatted value
+     * @throws InvalidConverterException
+     */
     public function format($converter, $datatype, $value, $options = [])
     {
         if ($converter == '') $converter = 'default';
@@ -48,32 +69,66 @@ class BaseMDH
         }
         throw new InvalidConverterException($converter);
     }
-
-    // convert the value from one converter to another
+    
+    /**
+     * Convert the value from one converter to another
+     * 
+     * @param string|null $converterFrom converter name or null to not parse the value
+     * @param string|null $converterTo converter name or null to not format the value
+     * @param string $datatype data type
+     * @param mixed $value value
+     * @param array $optionsFrom options to parse
+     * @param array $optionsTo options to format
+     * @return mixed converted value
+     */
     public function convert($converterFrom, $converterTo, $datatype, $value, $optionsFrom = [], $optionsTo = [])
     {
-        return $this->format($converterTo, $datatype, $this->parse($converterFrom, $datatype, $value, $optionsFrom), $optionsTo);
+        if ($converterFrom !== null)
+            $value = $this->parse($converterFrom, $datatype, $value, $optionsFrom);
+        if ($converterTo !== null)
+            $value = $this->format($converterTo, $datatype, $value, $optionsTo);
+        return $value;
     }
     
-    // gets a converter
+    /**
+     * Gets a converter
+     * 
+     * @param string $name converter name
+     * @return IConverter converter
+     */
     public function getConverter($name)
     {
         return $this->_converters[$name];
     }
     
-    // adds a new converter
+    // 
+    /**
+     * Adds a converter
+     * 
+     * @param string $name converter name
+     * @param IConverter $converter converter
+     */
     public function addConverter($name, $converter)
     {
         $this->_converters[$name] = $converter;
     }
     
-    // gets the locale to use when needed
+    /**
+     * Gets the locale
+     * 
+     * @return string
+     */
     public function getLocale()
     {
         return 'en-US';
     }
     
-    // get converter as property
+    /**
+     * Get converter handler as property
+     * 
+     * @param string $name converter name
+     * @return BaseMDHConverter converter handler
+     */
     public function __get($name)
     {
         if (isset($this->_converters[$name])) {
